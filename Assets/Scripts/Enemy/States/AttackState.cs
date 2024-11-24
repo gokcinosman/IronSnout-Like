b8b1354cbf6096d1720000f3a10b03651e5ba46a
Gokcin Osman <osmangokcinn@gmail.com>
2024-11-24 13:33:07 +0300

    warning fix

--- Assets/Scripts/Enemy/States/AttackState.cs
+++ Assets/Scripts/Enemy/States/AttackState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackState : MonoBehaviour, IState
+public class AttackState : IState
 {
     public void EnterState(IEnemy enemy)
     {
